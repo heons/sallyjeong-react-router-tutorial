bef7ff6edb76313500a1b1aa94d16a784d3ac195
HUEN_OH <heons921@gmail.com>
2022-05-20 21:48:32 -0400

    add about link

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,11 @@
+import { Link } from "react-router-dom";
+
 const Home = () => {
   return (
     <div>
       <h1>Home</h1>
       <p>The first page of the application.</p>
+      <Link to="/about">Go to about page</Link>
     </div>
   );
 };
