22eb24b7283e3e21fac31d403ee3e7e10ac293bf
HUEN_OH <heons921@gmail.com>
2022-06-03 21:16:33 -0400

    outlet and route under route

--- src/pages/Articles.js
+++ src/pages/Articles.js
@@ -1,18 +1,21 @@
-import { Link } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 const Articles = () => {
   return (
-    <ul>
-      <li>
-        <Link to="/articles/1">Article 1</Link>
-      </li>
-      <li>
-        <Link to="/articles/2">Article 2</Link>
-      </li>
-      <li>
-        <Link to="/articles/3">Article 3</Link>
-      </li>
-    </ul>
+    <div>
+      <ul>
+        <li>
+          <Link to="/articles/1">Article 1</Link>
+        </li>
+        <li>
+          <Link to="/articles/2">Article 2</Link>
+        </li>
+        <li>
+          <Link to="/articles/3">Article 3</Link>
+        </li>
+      </ul>
+      <Outlet />
+    </div>
   );
 };
 
