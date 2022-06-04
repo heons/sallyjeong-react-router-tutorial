1639b7e9cb9dbbccfc31dfef49634d849786093f
HUEN_OH <heons921@gmail.com>
2022-06-03 21:30:59 -0400

    useNavigate

--- src/Layout.js
+++ src/Layout.js
@@ -1,10 +1,20 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 
 const Layout = () => {
+  const navigate = useNavigate();
+
+  const goBack = () => {
+    navigate(-1);
+  };
+  const goArticle = () => {
+    navigate("/articles");
+  };
+
   return (
     <div>
       <header style={{ backgroundColor: "#f5f5f5", padding: 16, fontSize: 24 }}>
-        Header
+        <button onClick={goBack}>Back</button>
+        <button onClick={goArticle}>Articles</button>
       </header>
       <main>
         <Outlet />
@@ -12,6 +22,5 @@ const Layout = () => {
     </div>
   );
 };
-
 
 export default Layout;
