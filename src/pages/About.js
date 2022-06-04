aefab3fed77d3591f594eb00037cbec98dfc5414
HUEN_OH <heons921@gmail.com>
2022-06-03 20:52:22 -0400

    useSearchParams

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,12 +1,29 @@
-import { useLocation } from "react-router-dom";
+import { useLocation, useSearchParams } from "react-router-dom";
 
 const About = () => {
   const location = useLocation();
   console.log(location);
+
+  const [searchParams, setSearchParams] = useSearchParams();
+  const detail = searchParams.get("detail");
+  const mode = searchParams.get("mode");
+
+  const onToogleDetail = () => {
+    setSearchParams({mode, detail: detail === 'true' ? 'false' : 'true'});
+  };
+
+  const onIncreaseMode = () => {
+    const nextMode = mode === null ? 1: parseInt(mode) + 1;
+    setSearchParams({mode: nextMode.toString(), detail});
+  };
   return (
     <div>
       <h1>About</h1>
       <p>About page of the application.</p>
+      <p>detail: {detail}</p>
+      <p>mode: {mode}</p>
+      <button onClick={onToogleDetail}>toggle detail</button>
+      <button onClick={onIncreaseMode}>increase mode</button>
     </div>
   );
 };
